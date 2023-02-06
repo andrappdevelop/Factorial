4d8150bcc9c7525c0270efb07b71a45c7033bd8b
IvanUsatov <andrappdevelop@gmail.com>
2023-02-06 19:11:11 +0700

    Add layout

--- app/src/main/java/com/example/factorial/MainActivity.kt
+++ app/src/main/java/com/example/factorial/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.factorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.factorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
